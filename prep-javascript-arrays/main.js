fb8474c0260b170fc6ae3152fc6286c488b0646b
James Aguirre <jamesaguirre98@gmail.com>
2023-03-03 07:01:50 +0000

    finished activity prep-js-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,10 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgress'];
+console.log(languages);
+const firstElement = languages[0];
+console.log(firstElement);
+const thirdElement = languages[2];
+console.log(thirdElement);
+const length = languages.length;
+console.log(length);
+const lastElement = languages[5];
+console.log(lastElement);
